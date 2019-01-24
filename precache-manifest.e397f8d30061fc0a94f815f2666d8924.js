9a0f7857e527c37161d0203f899fdd8eb319b2f1
Viktor Abakumov <viktor.abakumov@algoritmika.org>
2019-01-24 23:17:59 +0500

    New lab

---
+++ precache-manifest.e397f8d30061fc0a94f815f2666d8924.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ebb60872523300bfa343613c220b1a4",
+    "url": "/static/media/icon-delete.6ebb6087.svg"
+  },
+  {
+    "revision": "6aef0a89b077e43204496653e1d7943d",
+    "url": "/static/media/icon-bin.6aef0a89.svg"
+  },
+  {
+    "revision": "08107bcf3756e2dfeaa8c8a6fb157637",
+    "url": "/static/media/bg.08107bcf.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15c0d76290c0c66c1998",
+    "url": "/static/js/main.15c0d762.chunk.js"
+  },
+  {
+    "revision": "1d56e2c8eaed935ba05c",
+    "url": "/static/js/1.1d56e2c8.chunk.js"
+  },
+  {
+    "revision": "15c0d76290c0c66c1998",
+    "url": "/static/css/main.4d8249f4.chunk.css"
+  },
+  {
+    "revision": "1d56e2c8eaed935ba05c",
+    "url": "/static/css/1.cef934ed.chunk.css"
+  },
+  {
+    "revision": "0195168d3567d5b686b065873c81d057",
+    "url": "/index.html"
+  }
+];
